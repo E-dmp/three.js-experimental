ba31e86d07dc3a76f1b53807eaffdbb940bad9fc
E-dmp <ridemdem1510@gmail.com>
2022-12-09 01:34:02 +0900

    test/@react-three/drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-
+import { Center, Float, Text, Text3D } from '@react-three/drei';
 function App() {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[0, 0, 0]} />
-    </Canvas>
-  </div>
+
+      <div className='textBox'>
+        <Canvas>
+
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[0, 0, 0]} />
+        </Canvas>
+      </div>
+    </div>
   )
 }
 
@@ -18,25 +22,8 @@ const Box = (props) => {
   const mesh = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  const [sizeX, setSizeX] = useState()
-  const [sizeY, setSizeY] = useState()
-  const [sizeZ, setSizeZ] = useState()
-  const [rotate, setRotate] = useState()
 
-  const RandomSize = () => {
-    let sizeX = Math.random()*2;
-    let sizeY = Math.random()*2.1;
-    let sizeZ = Math.random()*3.3;
-    setSizeX(sizeX)
-    setSizeY(sizeY)
-    setSizeZ(sizeZ)
-  }
- 
-setInterval(RandomSize, 6000);
-
-
-  useFrame(() => (mesh.current.rotation.x += 1));
-  useFrame(() => (mesh.current.rotation.y -= 2));
+  useFrame(() => (mesh.current.rotation.x += 0.02));
 
   return (
     <mesh
@@ -47,11 +34,30 @@ setInterval(RandomSize, 6000);
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
-      <boxGeometry args={[sizeX, sizeY, sizeZ]} />
-      <meshStandardMaterial color={hovered ? 'red' : 'orange'} />
+      <boxGeometry args={[4, 0.11, 3]} />
+      <meshStandardMaterial color={hovered ? 'orange' : 'orange'} />
+      <Text
+        position={[0, 0, 0]}
+        font="../src/font/nn.ttf"
+        fontSize={1}
+        color={'#FFF'}
+      >
+        ここ最近試験的に何個かアプリを作って分かったのはTypeScriptの理解度が低すぎる😂
+
+      </Text>
+      <Text
+        position={[0, -1, 0]}
+        font="../src/font/nn.ttf"
+        fontSize={1}
+        color={'#FFF'}
+      >
+        ノリで行けるでしょ！と思ってた自分が恥ずかしい..
+      </Text>
     </mesh>
   );
 };
+
+
 
 
 
